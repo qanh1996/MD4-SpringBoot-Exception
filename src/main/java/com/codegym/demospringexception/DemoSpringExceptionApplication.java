de37ee6830b74859e79426e29188055a70a8d009
unknown <phamthulien1975@gmail.com>
2022-06-20 00:29:30 +0700

    up

---
+++ src/main/java/com/codegym/demospringexception/DemoSpringExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.demospringexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/demospringexception/DemoSpringExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.demospringexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
